58e9b7d95c1792aef643f3e79a791d916128b16b
Cleophus Robinson IV <CleoR41@gmail.com>
2015-02-16 17:09:00 -0800

    Created the flow of the website

--- src/login/LoginServlet.java
+++ src/login/LoginServlet.java
@@ -8,11 +8,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/login")
+@WebServlet({"/","/login"})
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -22,19 +23,47 @@ public class LoginServlet extends HttpServlet {
     public LoginServlet() {
         super();
     }
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("username") != null){
+			//Already logged in, send to user page
+		}
+		if(request.getAttribute("errors") == null){
+			//There are no errors
+			request.setAttribute("title","");
+			request.setAttribute("message","");
+		}else{
+			//There was an error logging in
+			request.setAttribute("title","");
+			request.setAttribute("message","");
+		}
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("username") != null){
+			//Already logged in, send to user page
+		}
 		ServletContext sc = getServletContext();
 		AccountManager am = (AccountManager)sc.getAttribute("accountManager");
 		String username = (String)request.getAttribute("username");
 		String password = (String)request.getAttribute("password");
 		if(am.checkCredentials(username,password)){
-			//Correct username and password
+			session.setAttribute("username",username);
+			request.removeAttribute("errors");
+			//Correct username and password, send to user page
 		}else{
+			request.setAttribute("errors","Bad username password combo");
 			//Incorrect username and password
+			doGet(request,response);
+			return;
 		}
 	}
 
