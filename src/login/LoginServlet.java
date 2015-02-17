f08cfeb227627c542392c53dc5b0b7b8d1b82359
Cleophus Robinson IV <CleoR41@gmail.com>
2015-02-16 17:20:15 -0800

    Everything works up to here

--- src/login/LoginServlet.java
+++ src/login/LoginServlet.java
@@ -2,6 +2,7 @@ package login;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,17 +31,21 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		if(session.getAttribute("username") != null){
-			//Already logged in, send to user page
+			RequestDispatcher dispatch = request.getRequestDispatcher("userhomeJSP.jsp");
+			dispatch.forward(request, response);
+			return;
 		}
 		if(request.getAttribute("errors") == null){
 			//There are no errors
-			request.setAttribute("title","");
-			request.setAttribute("message","");
+			request.setAttribute("title","Welcome to Homework 5");
+			request.setAttribute("message","Please log in.");
 		}else{
 			//There was an error logging in
-			request.setAttribute("title","");
-			request.setAttribute("message","");
+			request.setAttribute("title","Please Try Again");
+			request.setAttribute("message","Either your username or password is incorrect. Please try again.");
 		}
+		RequestDispatcher dispatch = request.getRequestDispatcher("loginJSP.jsp");
+		dispatch.forward(request, response);
 	}
 
 	/**
@@ -59,11 +64,12 @@ public class LoginServlet extends HttpServlet {
 			session.setAttribute("username",username);
 			request.removeAttribute("errors");
 			//Correct username and password, send to user page
+			RequestDispatcher dispatch = request.getRequestDispatcher("userhomeJSP.jsp");
+			dispatch.forward(request, response);
 		}else{
 			request.setAttribute("errors","Bad username password combo");
 			//Incorrect username and password
 			doGet(request,response);
-			return;
 		}
 	}
 
