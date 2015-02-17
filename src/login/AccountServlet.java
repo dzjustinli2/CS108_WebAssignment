ee61b0a64f60c5b2383a47d6a62687e2c9e054a7
Cleophus Robinson IV <CleoR41@gmail.com>
2015-02-16 16:28:41 -0800

    doPost is set up for both servlets

---
+++ src/login/AccountServlet.java
@@ -0,0 +1,41 @@
+package login;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AccountServlet
+ */
+@WebServlet("/signup")
+public class AccountServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AccountServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext sc = getServletContext();
+		AccountManager am = (AccountManager)sc.getAttribute("accountManager");
+		String username = (String)request.getAttribute("username");
+		String password = (String)request.getAttribute("password");
+		if(am.addUser(username,password)){
+			//Username is not taken
+		}else{
+			//Username is taken
+		}
+	}
+
+}
